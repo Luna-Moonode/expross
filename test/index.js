d1b6f587e8390ae101a2682ad57f1b37f6e8bd51
Moonode <55302867+Luna-Moonode@users.noreply.github.com>
2020-01-23 16:30:52 +0800

    Add files via upload

--- test/index.js
+++ test/index.js
@@ -2,9 +2,16 @@ const expross = require('../');
 const app = expross();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req, res, next) => {
+    next();
+}).get('/', (req, res, next) => {
+    next(new Error('index err'))
 });
+// app.post('/post', (req, res, next) => {
+//     res.send('post');
+// });
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
